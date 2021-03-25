ea724877754ebc8cefd46502f5051d49d1977b9e
Tywayne <tywaynebuchanan@gmail.com>
2021-03-25 08:27:10 -0500

    changed js

--- contact.js
+++ contact.js
@@ -6,7 +6,7 @@ const icon = document.querySelector('.icon');
 function checkError(){
     if(inputBox.value == null|| inputBox.value == ""){
         inputBox.classList.add("error");
-        icon.style.display = "block";
+        icon.style.display = "inline";
         message.innerText = "Please enter a valid Your project's Zip Code"
         
        
@@ -18,7 +18,7 @@ btn.addEventListener('click',checkError)
 const clearStyle =()=>{
     inputBox.classList.remove("error");
     message.innerText = "";
-    // icon.classList.add('far fa-check-circle');
+   
     icon.style.display = "none";
 }
 
