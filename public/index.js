d3f71280fe9b7f772c0809701f26614d040ca57c
Tywayne <tywaynebuchanan@gmail.com>
2021-03-28 08:50:40 -0500

    changed logo

--- public/index.js
+++ public/index.js
@@ -1,3 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC4YlSa44tY9NbB7b2FSYOyfKUwOiGAA5A",
+    authDomain: "rubi-ba56e.firebaseapp.com",
+    projectId: "rubi-ba56e",
+    storageBucket: "rubi-ba56e.appspot.com",
+    messagingSenderId: "918738900993",
+    appId: "1:918738900993:web:bea5678feb2ebf62838800",
+    measurementId: "G-DLNEZERL6X"
+  };
+  
 const close = document.getElementById('closebtn');
 const menu = document.getElementById('drop_menu');
 const menudown = document.getElementById('menudown');
